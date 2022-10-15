4be79456b24f0d45b1f6c49474a5e7ffa55e6f1d
kurth4cker <kurth4cker@tutanota.com>
2022-10-15 13:18:42 +0300

    mangala.c: add initgame() and game_t

--- mangala.c
+++ mangala.c
@@ -2,10 +2,21 @@
 
 #include "board.h"
 
+struct game {
+	int board[2][7];
+	int nrock;
+	int *iter;
+	int *user;
+};
+typedef struct game game_t;
+
+static void initgame(game_t *);
+
 int
 main()
 {
 	WINDOW *board;
+	game_t game;
 
 	initscr();
 	noecho();
@@ -16,6 +27,19 @@ main()
 	initboard(board);
 	wrefresh(board);
 
+	initgame(&game);
+
 	getch();
 	endwin();
+}
+
+static void
+initgame(game_t *game)
+{
+	int i;
+	for (i = 0; i < 6; i++) {
+		game->board[0][i] = 4;
+		game->board[1][i] = 4;
+	}
+	game->user = game->board[0];
 }

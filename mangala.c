3e7729a05696705e172f8804272a381f52a3b038
kurth4cker <kurth4cker@tutanota.com>
2022-10-16 17:05:39 +0300

    mangala.c: add new window and main gameplay loop

--- mangala.c
+++ mangala.c
@@ -16,21 +16,42 @@ int
 main()
 {
 	WINDOW *board;
+	WINDOW *bottom;
 	game_t game;
+	int ch;
+	int maxx;
 
 	initscr();
 	noecho();
 	curs_set(0);
 
+	maxx = getmaxx(stdscr);
+
 	refresh();
-        board = newwin(7, 25, 5, 5);
+	board = newwin(7, 25, 5, 5);
+	bottom = newwin(4, maxx, 13, 3);
 
 	initboard(board);
 	initgame(&game);
 	fillboard(board, game.board);
 	wrefresh(board);
 
-	getch();
+	mvwaddstr(bottom, 0, 0, "press a number between 1 and 6 to select a XXX");
+	wrefresh(bottom);
+	while ((ch = getch()) != 'q') {
+		ch -= 48;
+		if (ch < 1 || ch > 6) {
+			mvwaddstr(bottom, 1, 0, "invalid selection. try again");
+			wrefresh(bottom);
+			continue;
+		}
+
+		/* here are some turn initing or playing actions */
+		/* and then redrawing of board window */
+		fillboard(board, game.board);
+		wrefresh(board);
+	}
+
 	endwin();
 }
 
